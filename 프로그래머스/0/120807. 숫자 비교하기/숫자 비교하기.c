a86308767b2b40f705e52da3a52a8dbb90316265
dmstlf1104 <75960436+dmstlf1104@users.noreply.github.com>
2024-01-16 20:51:16 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.44 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    if(0<= num1 && num1<=10000);
+    if(0<= num1 && num1<=10000);
+    int answer = 0;
+    if (num1 > num2)
+    {
+        answer = -1;
+    } else if(num1 < num2) {
+        answer = -1;
+    } else {
+        answer =1;
+    }
+    return answer;
+    
+}
